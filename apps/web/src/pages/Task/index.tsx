6263e516012b236f6535f6c34b67f585ce049a67
Merv Tampus <79685609+jdesolate@users.noreply.github.com>
2023-03-27 21:36:45 +0800

    [Adjustment] Ticket Page Grid Responsiveness

--- apps/web/src/pages/Task/index.tsx
+++ apps/web/src/pages/Task/index.tsx
@@ -1,10 +1,11 @@
-import { Button, Image, Group, Paper, SimpleGrid, Stack, Text, Title, Grid } from '@mantine/core';
+import { Button, Image, Group, Paper, SimpleGrid, Stack, Text, Title, Grid, Container } from '@mantine/core';
 import { IconArrowRight } from '@tabler/icons';
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import CustomLoader from 'components/CustomLoader';
 
+import TaskStatus from 'enums/TaskStatus';
 import { Task } from 'types/Task';
 
 import * as S from '../styles';
@@ -45,90 +46,92 @@ function TaskPage() {
 
   const renderLoading = loading && <CustomLoader />;
 
+  const renderImages = <Paper withBorder>
+    <Image
+      withPlaceholder
+      alt="With custom placeholder"
+      height={120}
+    />
+  </Paper>;
+
+  const renderToDo = <Paper withBorder px={25}>
+    <p>Setup migrations</p>
+  </Paper>;
+
   return (
     <S.PageContainer>
-      {renderLoading}
-      <Group position="right">
-        <Button color="gray" m={10} onClick={handleBack}>Back</Button>
-      </Group>
-      <Grid gutter="xl" mx={50} my={30}>
-        <Grid.Col span={8}>
-          <Stack align="stretch">
-            <Text size={36} weight={800}>{task?.name}</Text>
-            <Group position="apart">
-              <Title size={24} weight={500}>Description</Title>
-              <Button>Upload Image</Button>
-            </Group>
-            <Paper withBorder px={25}>
-              <p>{task?.description}</p>
-            </Paper>
-            <SimpleGrid cols={3}>
-              <Paper withBorder>
-                <Image
-                  withPlaceholder
-                  alt="With custom placeholder"
-                  height={120}
-                />
-              </Paper>
-              <Paper withBorder>
-                <Image
-                  withPlaceholder
-                  alt="With custom placeholder"
-                  height={120}
-                />
-              </Paper>
-              <Paper withBorder>
-                <Image
-                  withPlaceholder
-                  alt="With custom placeholder"
-                  height={120}
-                />
-              </Paper>
-            </SimpleGrid>
-            <Group position="apart">
-              <Title size={24} weight={500}>To Dos</Title>
-              <Button>Add</Button>
-            </Group>
-            <Paper withBorder px={25}>
-              <p>Setup migrations</p>
-            </Paper>
-            <Paper withBorder px={25}>
-              <p>Deploy backend</p>
-            </Paper>
-            <Group position="apart">
-              <Title size={24} weight={500}>Comments</Title>
-              <Button>Add new comment</Button>
-            </Group>
-            <Paper withBorder px={25} py={15}>
+      <Paper p="xl">
+        {renderLoading}
+        <Group position="right">
+          <Button color="gray" m={10} onClick={handleBack}>Back</Button>
+        </Group>
+        <Grid>
+          <Grid.Col md={8}>
+            <Stack align="stretch">
+              <Text size={36} weight={800}>{task?.name}</Text>
               <Group position="apart">
-                <Text>All details are subject to change</Text>
-                <Text color="blue">Merv Tampus</Text>
+                <Title size={24} weight={500}>Description</Title>
+                <Button>Upload Image</Button>
               </Group>
-            </Paper>
-          </Stack>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Stack>
-            <Title mt={70} size={24} weight={500}>Status</Title>
-            <Paper withBorder mt={5} pl={25} pr={10}>
+              <Paper withBorder px={25}>
+                <p>{task?.description ?? 'Not Available'}</p>
+              </Paper>
+              <SimpleGrid
+                breakpoints={[
+                  { cols: 1, maxWidth: 'xs' },
+                  { cols: 2, maxWidth: 'sm' },
+                  { cols: 3, maxWidth: 'md' },
+                ]}
+                cols={3}>
+                {renderImages}
+                {renderImages}
+                {renderImages}
+              </SimpleGrid>
+            </Stack>
+          </Grid.Col>
+          <Grid.Col md={4}>
+            <Stack>
+              <Title mt={15} size={24} weight={500}>Status</Title>
+              <Paper withBorder mt={5} pl={25} pr={10}>
+                <Group position="apart">
+                  <p>{task?.status ?? TaskStatus.IN_PROGRESS}</p>
+                  <Button size="sm" variant="white">
+                    <IconArrowRight
+                      color={'#228BE6'}
+                      size={24}
+                      strokeWidth={2}
+                    />
+                  </Button>
+                </Group>
+              </Paper>
+              <Title size={24} weight={500}>Assigned</Title>
+              <S.AssignedContainer>
+                <p>Merv Tampus</p>
+              </S.AssignedContainer>
+            </Stack>
+          </Grid.Col>
+          <Grid.Col md={8}>
+            <Stack>
               <Group position="apart">
-                <p>{task?.status}</p>
-                <Button size="sm" variant="white">
-                  <IconArrowRight
-                    color={'#228BE6'}
-                    size={24}
-                    strokeWidth={2}
-                  />
-                </Button>
+                <Title size={24} weight={500}>To Dos</Title>
+                <Button>Add</Button>
               </Group>
-            </Paper>
-            <Title size={24} weight={500}>Assigned</Title>
-            <S.AssignedContainer>
-              <p>Merv Tampus</p>
-            </S.AssignedContainer>
-          </Stack>
-        </Grid.Col>
-      </Grid>
+              {renderToDo}
+              {renderToDo}
+              <Group position="apart">
+                <Title size={24} weight={500}>Comments</Title>
+                <Button>Add new comment</Button>
+              </Group>
+              <Paper withBorder px={25} py={15}>
+                <Group position="apart">
+                  <Text>All details are subject to change</Text>
+                  <Text color="blue">Merv Tampus</Text>
+                </Group>
+              </Paper>
+            </Stack>
+          </Grid.Col>
+        </Grid>
+      </Paper>
     </S.PageContainer>
   );
 }
